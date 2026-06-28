f858821c92c398ca2db5080f7cf203aa4fe98b93
divyanshugit26 <divyanshu.pal2025@glbajajgroup.org>
2026-06-28 15:46:23 +0530

    Day-20 assignment has submitted

---
+++ Day-20/que-78.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2], flag=1;
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            scanf("%d",&a[i][j]);
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            if(a[i][j]!=a[j][i])
+                flag=0;
+
+    if(flag)
+        printf("Symmetric");
+    else
+        printf("Not Symmetric");
+
+    return 0;
+}
---
+++ Day-20/que-79.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2], sum;
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            scanf("%d",&a[i][j]);
+
+    for(int i=0;i<2;i++){
+        sum=0;
+        for(int j=0;j<2;j++)
+            sum+=a[i][j];
+
+        printf("%d\n",sum);
+    }
+
+    return 0;
+}
