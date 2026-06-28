9bb635db96400a7bb360b4f3909cfbb690c89bb3
divyanshugit26 <divyanshu.pal2025@glbajajgroup.org>
2026-06-28 15:24:36 +0530

    Day-19n assignment has submitted

---
+++ Day-19/que-75.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a[2][2];
+
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            scanf("%d",&a[i][j]);
+
+    printf("Transpose:\n");
+
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++)
+            printf("%d ",a[j][i]);
+        printf("\n");
+    }
+
+    return 0;
+}
